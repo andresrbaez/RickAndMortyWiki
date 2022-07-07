d459f26976f5160758e121db2c2c378784f09482
andresrbaez <andres.baez24@hotmail.com>
2022-07-07 10:01:54 -0500

    añadiendo datos de clase

---
+++ src/components/RickAndMortys.jsx
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+const RickAndMortys = () => {
+
+    const [getInfo, setGetInfo] = useState({});
+    const [searchValue, setSearchValue] = useState("")
+    const [urls, setUrls] = useState([]);
+    const random = Math.floor(Math.random() * 126) + 1
+
+    useEffect(() => {
+        axios.get(`https://rickandmortyapi.com/api/location/${random}`)
+            .then(res => setGetInfo(res.data))
+            // setUrls(res.data)
+    }, []);
+    useEffect(() => {
+        axios.get(`https://rickandmortyapi.com/api/location/${random}`)
+            .then(res => setUrls(res.data.residents))
+            // setUrls(res.data)
+    }, []);
+
+    const searchType = () => {
+        axios.get(`https://rickandmortyapi.com/api/location/${searchValue}`)
+            .then(res => setGetInfo(res.data))
+    }
+
+    console.log(getInfo);
+    // console.log(urls);
+
+
+
+    return (
+        <div>
+            <div className='input-div'>
+                <input type="text" placeholder='escriba id' value={searchValue} onChange={e => setSearchValue(e.target.value)}/>
+                <button onClick={searchType} className='btn-RNM'>Search</button>
+            </div>
+                <h1>{getInfo.name}</h1>
+                <ul>
+                        {getInfo.residents?.map((url) => (
+                            <li>
+                                {url}
+                            </li>
+                        ))}
+                </ul>
+
+
+            
+        </div>
+    );
+};
+
+export default RickAndMortys;
