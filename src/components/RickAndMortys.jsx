6ea4be6b0ac06d67dce8bf71f58ab8689254d7b0
andresrbaez <andres.baez24@hotmail.com>
2022-07-07 20:32:14 -0500

    correcciones del profesor

--- src/components/RickAndMortys.jsx
+++ src/components/RickAndMortys.jsx
@@ -1,53 +1,52 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const RickAndMortys = () => {
+  const [getInfo, setGetInfo] = useState({});
+  const [searchValue, setSearchValue] = useState("");
+  const random = Math.floor(Math.random() * 126) + 1;
 
-    const [getInfo, setGetInfo] = useState({});
-    const [searchValue, setSearchValue] = useState("")
-    const random = Math.floor(Math.random() * 126) + 1
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/location/${random}`)
+      .then((res) => {
+        setGetInfo(res.data);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get(`https://rickandmortyapi.com/api/location/${random}`)
-            .then(res => {
-                setGetInfo(res.data);
-              });
-    }, []);
+  const searchType = (e) => {
+    e.preventDefault();
+    axios
+      .get(`https://rickandmortyapi.com/api/location/${searchValue}`)
+      .then((res) => setGetInfo(res.data));
+  };
 
-    const searchType = () => {
-        axios.get(`https://rickandmortyapi.com/api/location/${searchValue}`)
-            .then(res => setGetInfo(res.data))
-    }
+  console.log(getInfo);
 
-    console.log(getInfo);
+  return (
+    <div className="principal-div">
+      <div className="search-div">
+        <form className="form" onSubmit={searchType}>
+          <input
+            type="text"
+            required
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <label className="lbl-name">
+            <span className="txt-name">Escriba ID</span>
+          </label>
+        </form>
+      </div>
 
-
-
-    return (
-        <div className='principal-div'>
-
-            <div className='search-div'>
-                <form className="form">
-                <input type="text" required value={searchValue} onChange={e => setSearchValue(e.target.value)}/>
-                <label className='lbl-name'>
-                    <span className="txt-name">
-                        Escriba ID
-                    </span>
-                </label>
-                </form>
-                <button onClick={searchType} className='btn-RNM'>Search</button>             
-            </div>
-
-                <h1>{getInfo.name}</h1>
-                <ul>
-                        {getInfo.residents?.map((url) => (
-                            <li>
-                                {url}
-                            </li>
-                        ))}
-                </ul>
-        </div>
-    );
+      <h1>{getInfo.name}</h1>
+      <ul>
+        {getInfo.residents?.map((url) => (
+          <li key={url}>{url}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default RickAndMortys;
